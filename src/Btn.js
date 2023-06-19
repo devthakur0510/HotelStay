c9531dea8e4a5237e8edb3f74323611f17df0a9f
devsendhav <devthakur0510@gmail.com>
2023-06-19 12:14:20 +0530

    same line code changes

--- src/Btn.js
+++ src/Btn.js
@@ -20,3 +20,5 @@ export default function Btn({bgColor, btnLabel, textColor, Press}) {
   );
 }
 // custom button implementation
+// Test comment to handle the conflict
+// Add comment in development branch
